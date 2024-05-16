2e93fc279be44d34f07a861c74877fe0ece32e20
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-16 15:17:52 +0100

    niyu

---
+++ lib/screens/splash.dart
@@ -0,0 +1,55 @@
+import 'package:dataapp/constant/constant.dart';
+import 'package:dataapp/helper/navigator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual);
+    Future.delayed(const Duration(seconds: 6), () {
+      //change the screen to the next screen
+      changeScreenReplacement(context, widget);
+    });
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              colors: [COLOR_BACKGROUND, Color.fromARGB(255, 219, 216, 216)],
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "images/logo.png",
+              width: 100,
+              height: 100,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
